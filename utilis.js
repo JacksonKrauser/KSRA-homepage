cf00ecb79a06dea7d59aeb42807c4e90d8d5e5e9
Jackson Krauser <jacksonkrauser@gmail.com>
2022-03-14 19:36:54 -0400

    second commit

--- utilis.js
+++ utilis.js
@@ -1,1 +1,28 @@
-utilis.js
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Thank you for submitting your interest");
+      return true;
+    }
+  }
+
+
+
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    console.log("hello")
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
