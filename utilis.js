9ef20b0b12b7ddffef534dd24b968c6b2413b830
Jackson Krauser <jacksonkrauser@gmail.com>
2022-03-28 21:21:02 -0400

    Eighth commit

--- utilis.js
+++ utilis.js
@@ -3,7 +3,7 @@ function validateForm() {
   let y = document.forms["myForm"]["fname"].value;
   let z = document.forms["myForm"]["lname"].value;
   if (x !== ""&& y !== "" && z !== "") {
-    document.getElementById("message").innerHTML="Success"
+    document.getElementById("message").innerHTML="Thank you for your interest.  A member from KSOA will contact you shortly."
     document.getElementById("message").style.display="Flex"
     document.forms["myForm"]["email"].value="";
     document.forms["myForm"]["fname"].value="";
